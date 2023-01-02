0be901f6ed315cfaa34b1d2d326d42baa8991d2b
Nafim-Ershad <inan.nafim1098@outlook.com>
2023-01-02 16:02:15 +0600

    Added some socket.io and styles css

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require('express');
-const io = require('socket.io');
+const app = express();
+const server = require('http').createServer(app); // SocketIO uses http instead of express
+const io = require('socket.io')(server); // COnnecting the SocketIO to my PORT number 
+const path = require('path');
+
+require('dotenv').config();
+
+
+const PORT = process.env.PORT || 8000;
+
+//MIDDLEWARE
+app.use(express.static(path.join(__dirname + '/static')));
+
+
+io.on('connection', socket => {
+    console.log('Socket Connected', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log(socket.id, 'has disconnected :(');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Listening to Port: ${PORT}`);
+});
